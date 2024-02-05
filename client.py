cf9deb747d57b9c4a0a9e15cf5573491fb48bdab
sodtiruk <sodtiruk22@gmail.com>
2024-02-06 02:51:23 +0700

    finish brief gui

--- client.py
+++ client.py
@@ -1,19 +1,40 @@
 import socket
 import threading
+from tkinter import *
 
 def main():
+    # Server and port that you want to connect to
     IPSERVER = "localhost"
     PORT = 3333
 
+    #connect server
     # client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     # client.connect((IPSERVER, PORT))
 
-         
+    gui = Tk()    
+    gui.title("Chat Online Basic") #set name title
+    gui.geometry("600x700") 
+    # gui.resizable(False, False)
+
+    textBoxReadChat = Text(gui, height=20, width=80, font=("Helvetica", 20))
+    textBoxTypeChat = Text(gui, height=10, width=20, font=("Helvetica", 32))    
+    buttonSendChat = Button(gui, text="Send", command=None, width=40, height=40)
 
 
+    #position widget
+    textBoxReadChat.pack()
+    textBoxTypeChat.pack(side=LEFT)
+    buttonSendChat.pack(side=RIGHT)
 
+    #bind keyboard enter and send
+    #when you press enter from your keyboard you will be able to send the message
+    gui.bind("<Return>", functionprint)
+
+
+    gui.mainloop()
     
-
+def functionprint(event):
+    print("hello world")
 
 if __name__ == "__main__":
     main()
